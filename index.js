08cea4dd89d7d9a3597a77911d91228257c1a1c8
ayberkaygun <56726603+ayberkaygun@users.noreply.github.com>
2023-05-31 05:41:53 +0300

    Update index.js
    index update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config({ path: "./.env" });
 const diabetesRoute = require("./routes/diabetes.js");
+const sendEmail = require("./security/security");
 
 const app = express();
 app.use(cors());
@@ -19,3 +20,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+sendEmail()
