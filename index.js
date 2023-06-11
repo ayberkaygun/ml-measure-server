67e7e25867ddde7b57df4919eb0e7a168d43d967
ayberkaygun <56726603+ayberkaygun@users.noreply.github.com>
2023-06-11 05:50:04 +0300

    Update index.js
    take mail from frontend

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config({ path: "./.env" });
 const diabetesRoute = require("./routes/diabetes.js");
-const sendEmail = require("./security/security");
-
+const security = require('./security/security.js');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,9 +15,16 @@ app.get("/", async (req, res) => {
 app.use("/diabetes", diabetesRoute);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.post('/send-email', (req, res) => {
+  const email = req.body.email;
+  
+  // E-postayı kullanarak yapmak istediğiniz işlemleri burada gerçekleştirin
+  security.sendEmail(email);
+  res.json({ message: 'E-posta başarıyla gönderildi!' });
 });
 
-sendEmail()
+app.listen(PORT, () => {
+  
+  console.log(`Listening on port ${PORT}`);
+  
+});
